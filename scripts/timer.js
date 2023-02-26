23e486b90dcceda2835c08762d60557c86e9f5cb
Chevron9 <ganymed.daniel@gmail.com>
2023-02-26 13:05:13 +0100

    timer as class

---
+++ scripts/timer.js
@@ -0,0 +1,13 @@
+class Timer {
+
+    constructor(duration, start, message) {
+        this.duration = duration
+        this.start = start
+        this.message = message
+
+        this.end = start+timer_duration;
+        console.log(`Timer ends at ${end}`)
+        this.end_date = new Date(end)
+        console.log(`${end_date}`)
+    }
+}
